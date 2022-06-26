fc39da3199d910a3ba05bd9a5b086f66a436d189
Abu-Github <106802706+Abu-Github@users.noreply.github.com>
2022-06-26 23:15:44 +0530

    Update setCaption.py

--- caption/setCaption.py
+++ caption/setCaption.py
@@ -18,9 +18,9 @@
 #Function for adding caption
 #to media such as audio,image,gifs and files
 def setCaption(update,context):
-  #if update.message.reply_to_message is not None:
-   #fileCaption= update.message.text
-   #fileType=update.message.reply_to_message
+  if update.message.reply_to_message is not None:
+   fileCaption= update.message.text
+   fileType=update.message.reply_to_message
    if fileType.document!=None:
      update.message.reply_document(
        update.message.reply_to_message.document.file_id,caption=fileCaption)
@@ -35,5 +35,5 @@ def setCaption(update,context):
        update.message.reply_to_message.voice.file_id,caption=fileCaption)
    elif fileType.photo!=None:
      update.message.reply_photo(update.message.reply_to_message.photo[-1].file_id,caption=fileCaption)
-  else:
-    update.message.reply_text(update.message.text)
+  #else:
+    #update.message.reply_text(update.message.text)
